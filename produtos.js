8b238c91b1df9c88fbb25182b1f7a22e1b44cfb3
rafaela <rafaela.pereira.melo@escola.pr.gov.br>
2024-10-03 12:34:11 -0300

    commit 4

--- produtos.js
+++ produtos.js
@@ -1,31 +1,51 @@
 let teucuzin = 0;
+let troca = 1;
 
-    function atualizar() {
-        const texto = document.getElementById("texto");
-        const items = document.querySelectorAll(".item");
-        const totalitems = items.length;
+const div = document.getElementById('ocult1');
+const div1 = document.getElementById('ocult2');
+function atualizarDivs() {
+    if (troca === 1) {
+        div.style.display = 'flex';
+        div1.style.display = 'none';
+    } else if (troca === 2) {
+        div.style.display = 'none';
+        div1.style.display = 'flex';
+    }
+}
 
-        const visivel = 1;
-        const max = totalitems - visivel;
+atualizarDivs();
 
-        if (teucuzin > max) {
-            teucuzin = max;
-        } else if (teucuzin < 0) {
-            teucuzin = 0;
-        }
+function atualizar() {
+    const texto = document.getElementById("texto");
+    const items = document.querySelectorAll(".item");
+    const totalitems = items.length;
 
-        texto.style.transform = `translateX(${-teucuzin * 200}px)`;
+    const visivel = 1;
+    const max = totalitems - visivel;
+
+    if (teucuzin > max) {
+        teucuzin = max;
+    } else if (teucuzin < 0) {
+        teucuzin = 0;
     }
 
-    function proximo() {
-        teucuzin++;
-        atualizar();
-    }
+    texto.style.transform = `translateX(${-teucuzin * 200}px)`;
+}
 
-    function anterior() {
-        teucuzin--;
-        atualizar();
-    }
-    function estado() {
-        
-    }
+
+function proximo() {
+    teucuzin++;
+    atualizar();
+    troca++;
+    if (troca > 2) troca = 2;
+    atualizarDivs();
+}
+
+function anterior() {
+    teucuzin--;
+    atualizar();
+    troca--;
+    if (troca < 1) troca = 1;
+    atualizarDivs();
+}
+console.log(troca);
